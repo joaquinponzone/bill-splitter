fcf7aedf0d5305163c78bbe7288fffec29fada13
joaquin <ponzonejoaquin@gmail.com>
2024-08-04 18:04:21 -0300

    chore: Add TailwindIndicator component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <TailwindIndicator />
       </body>
     </html>
   );
